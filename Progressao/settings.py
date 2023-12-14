32944d0a91951d39d87d7a4b8d3a5c8d4d2dd4fc
ThiSouza <128190533+ThiSouza21@users.noreply.github.com>
2023-12-14 18:24:19 -0300

    Update settings.py

--- Progressao/settings.py
+++ Progressao/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'organizaai',
         'USER': 'root',
-        'PASSWORD': 'ThiVini2!0206',
+        'PASSWORD': '',
         'HOST':'localhost',
         'PORT':'3306',
     }
